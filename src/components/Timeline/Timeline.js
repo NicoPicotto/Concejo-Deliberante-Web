f0c34f91533ad2a1de33ced8a08513cbe0c54711
NicoPicotto <nicopicotto@hotmail.com>
2022-03-26 02:21:38 -0300

    Timeline con librería

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,8 +1,233 @@
 import React from 'react';
-import './TimelineElements';
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 const Timeline = () => {
-	return <div className='timeline-container'></div>;
+	return (
+		<div className='timeline-container'>
+			<VerticalTimeline>
+				<VerticalTimelineElement
+					className='vertical-timeline-element--work'
+					contentStyle={{ background: '#fff', color: 'black' }}
+					contentArrowStyle={{ borderRight: '7px solid  #fff' }}
+					date='2019 - Presente'
+					iconStyle={{ background: '#febe39', color: '#fff' }}
+				>
+					<h3 className='vertical-timeline-element-title'>Marcos G. Carasso</h3>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>Concejales</h4>
+					<p>
+						GUILLERMO D. CAVIGLIASSO <br />
+						EDGARDO J. TAMBURELLI
+						<br />
+						SILVINA ANGELETTI
+						<br />
+						DEBORA S. COTS. REEMPLAZADA POR MARIANA M. MUSSO
+						<br />
+						JUAN PABLO MACAGNO
+						<br />
+						MAURO N. SALUSSO. REEPLAZADO POR MAURO CAMPAGNO
+						<br />
+						CLAUDIA S. VILLARRUEL
+					</p>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>
+						Tribunal de cuentas
+					</h4>
+					<p>
+						CARLOS R. BOSSIO
+						<br />
+						GISELE L. GATICA
+						<br />
+						ALEJANDRO J. MASSOBRIO
+					</p>
+				</VerticalTimelineElement>
+				<VerticalTimelineElement
+					className='vertical-timeline-element--work'
+					contentStyle={{ background: '#fff', color: 'black' }}
+					contentArrowStyle={{ borderRight: '7px solid  #fff' }}
+					date='2019 - Presente'
+					iconStyle={{ background: '#febe39', color: '#fff' }}
+				>
+					<h3 className='vertical-timeline-element-title'>Marcos G. Carasso</h3>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>Concejales</h4>
+					<p>
+						GUILLERMO D. CAVIGLIASSO <br />
+						EDGARDO J. TAMBURELLI
+						<br />
+						SILVINA ANGELETTI
+						<br />
+						DEBORA S. COTS. REEMPLAZADA POR MARIANA M. MUSSO
+						<br />
+						JUAN PABLO MACAGNO
+						<br />
+						MAURO N. SALUSSO. REEPLAZADO POR MAURO CAMPAGNO
+						<br />
+						CLAUDIA S. VILLARRUEL
+					</p>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>
+						Tribunal de cuentas
+					</h4>
+					<p>
+						CARLOS R. BOSSIO
+						<br />
+						GISELE L. GATICA
+						<br />
+						ALEJANDRO J. MASSOBRIO
+					</p>
+				</VerticalTimelineElement>
+				<VerticalTimelineElement
+					className='vertical-timeline-element--work'
+					contentStyle={{ background: '#fff', color: 'black' }}
+					contentArrowStyle={{ borderRight: '7px solid  #fff' }}
+					date='2019 - Presente'
+					iconStyle={{ background: '#febe39', color: '#fff' }}
+				>
+					<h3 className='vertical-timeline-element-title'>Marcos G. Carasso</h3>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>Concejales</h4>
+					<p>
+						GUILLERMO D. CAVIGLIASSO <br />
+						EDGARDO J. TAMBURELLI
+						<br />
+						SILVINA ANGELETTI
+						<br />
+						DEBORA S. COTS. REEMPLAZADA POR MARIANA M. MUSSO
+						<br />
+						JUAN PABLO MACAGNO
+						<br />
+						MAURO N. SALUSSO. REEPLAZADO POR MAURO CAMPAGNO
+						<br />
+						CLAUDIA S. VILLARRUEL
+					</p>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>
+						Tribunal de cuentas
+					</h4>
+					<p>
+						CARLOS R. BOSSIO
+						<br />
+						GISELE L. GATICA
+						<br />
+						ALEJANDRO J. MASSOBRIO
+					</p>
+				</VerticalTimelineElement>
+                <VerticalTimelineElement
+					className='vertical-timeline-element--work'
+					contentStyle={{ background: '#fff', color: 'black' }}
+					contentArrowStyle={{ borderRight: '7px solid  #fff' }}
+					date='2019 - Presente'
+					iconStyle={{ background: '#febe39', color: '#fff' }}
+				>
+					<h3 className='vertical-timeline-element-title'>Marcos G. Carasso</h3>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>Concejales</h4>
+					<p>
+						GUILLERMO D. CAVIGLIASSO <br />
+						EDGARDO J. TAMBURELLI
+						<br />
+						SILVINA ANGELETTI
+						<br />
+						DEBORA S. COTS. REEMPLAZADA POR MARIANA M. MUSSO
+						<br />
+						JUAN PABLO MACAGNO
+						<br />
+						MAURO N. SALUSSO. REEPLAZADO POR MAURO CAMPAGNO
+						<br />
+						CLAUDIA S. VILLARRUEL
+					</p>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>
+						Tribunal de cuentas
+					</h4>
+					<p>
+						CARLOS R. BOSSIO
+						<br />
+						GISELE L. GATICA
+						<br />
+						ALEJANDRO J. MASSOBRIO
+					</p>
+				</VerticalTimelineElement>
+                <VerticalTimelineElement
+					className='vertical-timeline-element--work'
+					contentStyle={{ background: '#fff', color: 'black' }}
+					contentArrowStyle={{ borderRight: '7px solid  #fff' }}
+					date='2019 - Presente'
+					iconStyle={{ background: '#febe39', color: '#fff' }}
+				>
+					<h3 className='vertical-timeline-element-title'>Marcos G. Carasso</h3>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>Concejales</h4>
+					<p>
+						GUILLERMO D. CAVIGLIASSO <br />
+						EDGARDO J. TAMBURELLI
+						<br />
+						SILVINA ANGELETTI
+						<br />
+						DEBORA S. COTS. REEMPLAZADA POR MARIANA M. MUSSO
+						<br />
+						JUAN PABLO MACAGNO
+						<br />
+						MAURO N. SALUSSO. REEPLAZADO POR MAURO CAMPAGNO
+						<br />
+						CLAUDIA S. VILLARRUEL
+					</p>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>
+						Tribunal de cuentas
+					</h4>
+					<p>
+						CARLOS R. BOSSIO
+						<br />
+						GISELE L. GATICA
+						<br />
+						ALEJANDRO J. MASSOBRIO
+					</p>
+				</VerticalTimelineElement>
+                <VerticalTimelineElement
+					className='vertical-timeline-element--work'
+					contentStyle={{ background: '#fff', color: 'black' }}
+					contentArrowStyle={{ borderRight: '7px solid  #fff' }}
+					date='2019 - Presente'
+					iconStyle={{ background: '#febe39', color: '#fff' }}
+				>
+					<h3 className='vertical-timeline-element-title'>Marcos G. Carasso</h3>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>Concejales</h4>
+					<p>
+						GUILLERMO D. CAVIGLIASSO <br />
+						EDGARDO J. TAMBURELLI
+						<br />
+						SILVINA ANGELETTI
+						<br />
+						DEBORA S. COTS. REEMPLAZADA POR MARIANA M. MUSSO
+						<br />
+						JUAN PABLO MACAGNO
+						<br />
+						MAURO N. SALUSSO. REEPLAZADO POR MAURO CAMPAGNO
+						<br />
+						CLAUDIA S. VILLARRUEL
+					</p>
+					<br />
+					<h4 className='vertical-timeline-element-subtitle'>
+						Tribunal de cuentas
+					</h4>
+					<p>
+						CARLOS R. BOSSIO
+						<br />
+						GISELE L. GATICA
+						<br />
+						ALEJANDRO J. MASSOBRIO
+					</p>
+				</VerticalTimelineElement>
+			</VerticalTimeline>
+		</div>
+	);
 };
 
 export default Timeline;
