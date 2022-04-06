88d2a6c8df98d063a185d567c4aa92ec3547b8e3
NicoPicotto <nicopicotto@hotmail.com>
2022-04-06 19:21:25 -0300

    Implementación digestos vigentes

--- src/components/Digestos/Digestos.js
+++ src/components/Digestos/Digestos.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
-import { AiFillFilePdf } from 'react-icons/ai';
+import { Card, Button, CardGroup } from 'react-bootstrap';
+import {
+	AiFillFilePdf,
+	AiFillCheckCircle,
+	AiFillCloseCircle,
+} from 'react-icons/ai';
 
 const Digestos = ({ data }) => {
 	const fecha = data.date.toDate().toLocaleDateString('es-ES');
@@ -14,7 +18,18 @@ const Digestos = ({ data }) => {
 				<Card.Body className='dig-card-body'>
 					<Card.Text className='dig-card-text'>{data.title}</Card.Text>
 					<Card.Text className='dig-card-tema'>Tema: {data.tema}</Card.Text>
-					<Card.Text className='dig-card-date'>Sancionada: {fecha}</Card.Text>
+					<CardGroup className='row-2'>
+						<Card.Text className='dig-card-date'>Sancionada: {fecha}</Card.Text>
+						{data.vigente ? (
+							<Card.Text className='dig-card-vigente'>
+								<AiFillCheckCircle /> Vigente
+							</Card.Text>
+						) : (
+							<Card.Text className='dig-card-vigente-false'>
+								<AiFillCloseCircle /> No Vigente
+							</Card.Text>
+						)}
+					</CardGroup>
 				</Card.Body>
 				<Button className='dig-card-download' href={data.link} target='_blank'>
 					Descargar PDF <AiFillFilePdf />
