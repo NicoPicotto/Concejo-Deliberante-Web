8bba4d4e6b8af0eda1183b2260bdbb04bdd6d61e
Nico Picotto <nicolaspicoto@Nicos-MacBook-Pro.local>
2024-03-13 21:09:13 -0300

    carta organica

--- src/components/InstitucionalLanding/InstitucionalLanding.js
+++ src/components/InstitucionalLanding/InstitucionalLanding.js
@@ -2,6 +2,7 @@ import React from 'react';
 import dots from '../../img/dots-01.png';
 import Lottie from 'lottie-react';
 import lottieHome from '../../Lottie/lf30_editor_krt6lqd2.json';
+import { Button } from 'react-bootstrap';
 
 const options = {
 	animationData: lottieHome,
@@ -20,7 +21,14 @@ const InstitucionalLanding = () => {
 					Se detallan los ciudadanos electos como intendentes y concejales
 					durante los mandatos correspondientes.
 				</h1>
-				<img src={dots} className='home-dots' alt="Puntos decorativos" />
+				<img src={dots} className='home-dots' alt='Puntos decorativos' />
+				<Button
+					href='./Carta_Organica_Municipal_MGC.pdf'
+					className='leg-button-2'
+					download
+				>
+					Descargar Carta Orgánica
+				</Button>
 			</div>
 			<div className='home-img-container'>
 				<Lottie {...options} />
